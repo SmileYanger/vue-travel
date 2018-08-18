c322eaa6915de78383ee2f4e6a88069e0fe8f0b3
unknown <1906479575@qq.com>
2018-08-18 15:58:29 +0800

    details list

---
+++ src/pages/details/components/list.vue
@@ -0,0 +1,41 @@
+<template>
+	<div>
+		<div class="list-box" v-for="(item,index) in lists" :key="index">
+			<div class="list-item border-bottom">
+				<span class="mp-ticketype-ticket"></span>
+				{{item.title}}
+			</div>
+			<div if="item.children">
+				<details-list :lists="item.children" class="children-item">
+				</details-list>
+			</div>
+			<!--递归组件的使用方法：就是自己用自己，通过：lists="item.children"使用，体现了details-list：组件的name-->
+		</div>
+	</div>
+</template>
+
+<script>
+	export default{
+		name:'DetailsList',
+		props:{
+			lists:Array
+		}
+	}
+</script>
+
+<style lang="stylus" scoped="scoped">
+.mp-ticketype-ticket 
+    display: inline-block
+    width: .36rem
+    height: .36rem
+    background: url(http://s.qunarzz.com/piao/image/touch/sight/detail.png) 0 -.45rem no-repeat
+    margin-right: .1rem
+    background-size: .4rem 3rem	
+    position:relative
+    top:.1rem
+.list-item
+	padding:.3rem
+.children-item
+	padding:0 .6rem
+
+</style>
--- src/pages/details/details.vue
+++ src/pages/details/details.vue
@@ -2,18 +2,60 @@
 	<div>
 		<details-banner></details-banner>
 		<details-header></details-header>
-		<div class="content"></div>
+		<div class="content">
+			<details-list :lists="list"></details-list>
+		</div>
 	</div>
 </template>
 
 <script>
 	import DetailsBanner from "./components/banner"
 	import DetailsHeader from "./components/header"
+	import DetailsList from "./components/list"
 	export default{
 		name:'ListDetails',
 		components:{
 			DetailsBanner,
-			DetailsHeader
+			DetailsHeader,
+			DetailsList
+		},
+		data(){
+			return{
+				list:[
+					{
+						title:'儿童票',
+						children:[
+							{
+								title:'双人票',
+									children:[
+										{title:'大唐芙蓉园-双人票'}
+									]
+								},
+							{
+								title:'三管连票',
+									children:[
+										{title:'华清池-三人票'}
+									]
+								},
+							{
+								title:'五管连票',
+									children:[
+										{title:'古城墙-五人票'}
+									]
+								}
+						]
+					},
+					{
+						title:'学生票'
+						},
+					{
+						title:'成人票'
+						},
+					{
+						title:'特惠票'
+						}
+				]
+			}
 		}
 	}
 </script>
